43c6d6bd3bddfb885bb6cb1a3143670b22bb8a86
PIeter <pieterverlinden2406@hotmail.com>
2018-12-30 23:05:52 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/logo.png">
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <AccountsOverview msg="Welcome to Your Vue.js App"/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import AccountsOverview from "./components/AccountsOverview.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld,
-  },
+    AccountsOverview
+  }
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30 px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/AccountsOverview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="accounts"></div>
+</template>
+
+<script>
+export default {
+  name: "AccountsOverview",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.accounts {
+  text-align: center;
+  height: 300px;
+}
+</style>
